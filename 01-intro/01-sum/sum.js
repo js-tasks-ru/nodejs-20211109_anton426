ff5aa522ff5d1247e42a1d15b3236780c5a0b764
SelivanovAE <selivanov@citk-parus.ru>
2021-11-26 17:01:43 +0300

    1st task

---
+++ 01-intro/01-sum/sum.js
@@ -0,0 +1,10 @@
+const { isString } = require("lodash");
+
+let sum = function(a,b){
+  if (isString(a)|isString(b)){
+    throw new TypeError()   
+    }else{
+      return a + b;  
+    }
+};
+module.exports = sum;
